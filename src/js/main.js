05eb1d508e6f3e80c7e2e73c59a162ed998aacde
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2017-01-16 19:29:27 +0100

    install router js

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,14 @@ require('../css/main.sass');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Edition from './pages/Edition';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 const app = document.getElementById("app");
-ReactDOM.render(<Edition />, app);
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Edition} />
+  </Router>, 
+  app);
 
 $('.chips').material_chip({
   placeholder: '+Tag',
