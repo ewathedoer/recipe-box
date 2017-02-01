7c9ad8ca534ada9c1aa7141fd0ea0225c5672294
Ewa Mitulska-Wójcik <thedoerdoes@gmail.com>
2017-02-01 21:05:06 +0100

    format detail view

--- src/js/components/Ingredients.js
+++ src/js/components/Ingredients.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 export default class Ingredients extends React.Component {
   render() {
-    return this.props.editable ?
+    if (this.props.editable) {
+      return 
       (
         <div className="input-field">
           <textarea id="textarea1" className="materialize-textarea" value={this.props.ingredients} onChange={this.props.onChange}></textarea>
           <label htmlFor="textarea1">Ingredients</label>
         </div>
-      ) :
-      (
-        <ul>
-          <li>
-            {this.props.ingredients}
-          </li>
-        </ul>
+      ); 
+    } else {
+      let str = this.props.ingredients;
+      str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+      return (
+        <p dangerouslySetInnerHTML={{__html: str}}>
+        </p>
       );
+    }
   }
 }
--- src/js/components/Instructions.js
+++ src/js/components/Instructions.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 export default class Instructions extends React.Component {
   render() {
-    return this.props.editable ? 
-      (
+    if (this.props.editable) {
+      return (
         <div className="input-field">
           <textarea id="textarea1" className="materialize-textarea" value={this.props.instructions} onChange={this.props.onChange}></textarea>
           <label htmlFor="textarea1">Instructions</label>
         </div>
-      ) :
-      (
-        <p>
-          {this.props.instructions}
+      );
+    } else {
+      let str= this.props.instructions;
+      str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+      return (
+        <p dangerouslySetInnerHTML={{__html: str}}>
         </p>
       );
+    }   
   }
 }
